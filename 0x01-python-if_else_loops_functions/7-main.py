87454bbf4adbf9ca990f11b50cd256275c2c5ed7
zahra613 <zahraaassila4@gmail.com>
2023-11-06 06:03:41 -0800

    my projects

---
+++ 0x01-python-if_else_loops_functions/7-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+islower = __import__('7-islower').islower
+
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
+
+guillaume@ubuntu:~/0x01$ ./7-main.py
+a is lower
+H is upper
+A is upper
+3 is upper
+g is lower
